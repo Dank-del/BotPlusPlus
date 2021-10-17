0425194dc2f6484b3bc0f9341ff038894421e595
Sayan Biswas <sayan@pokurt.me>
2021-10-18 01:39:32 +0530

    info.cpp: remove useless import
    Signed-off-by: Sayan Biswas <sayan@pokurt.me>

--- cmds/info.cpp
+++ cmds/info.cpp
@@ -1,14 +1,10 @@
-#include <mdparser-cpp/WotoMd.h>
-#include <mdparser-cpp/WotoMd.cpp>
-#include <tgbot/tgbot.h>
-#include <boost/format.hpp>
 #include <utility>
 #include "../utils/getuser.hpp"
 
 namespace msg {
-    std::string infohandler(TgBot::Message::Ptr message){
+    std::string infohandler(const TgBot::Message::Ptr& message){
     auto msg = get_bold("User info:")->AppendNormal("\n");
-    auto user = user::get(std::move(message));
+    auto user = user::get(message);
     auto id = boost::format("%1%") % user->id;
     msg = msg->AppendNormal("First Name")->AppendNormal(": ")->AppendNormal(user->firstName)->AppendNormal("\n");
     msg = msg->AppendNormal("Last Name")->AppendNormal(": ")->AppendNormal(user->lastName)->AppendNormal("\n");
